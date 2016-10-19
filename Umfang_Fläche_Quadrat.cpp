243aac781eab72b8f913533d20f0e3e121083f45
Steffen <steffen.moeller1993@gmx.net>
2016-10-19 14:25:05 +0200

    Update Umfang_Fläche_Quadrat.cpp

--- Umfang_Fläche_Quadrat.cpp
+++ Umfang_Fläche_Quadrat.cpp
@@ -5,13 +5,13 @@ int main()
 {
 	double seitenlaenge;
 	// Ausgabe des Textes
-	cout << "Bitte geben sie die Seitenlaenge in cm ein:";
+	cout << "Bitte geben Sie die Seitenlaenge ein (in cm): ? ";
 	// Hier soll die Seitenlaenge eingegeben werden
 	cin >> seitenlaenge;
 	// Programm zur Ausgabe des Umfangs
-	cout << "Der Umfang betraegt in cm: " << 4 * seitenlaenge << endl;
-	// Programm zur Berechnung des Flächeninhalts
-	cout << "Die Flaeche des Quadrats betraegt in cm*cm: " << seitenlaenge * seitenlaenge << endl;
+	cout << "Der Umfang des Quadrats betraegt (in cm): " << 4 * seitenlaenge << endl;
+	// Programm zur Berechnung des FlÃ¤cheninhalts
+	cout << "Die Flaeche des Quadrats betraegt (in cm*cm): " << seitenlaenge * seitenlaenge << endl;
 
 	system("PAUSE");
 
